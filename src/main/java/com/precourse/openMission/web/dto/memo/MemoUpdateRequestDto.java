f13bc6db32adb823b88a63b0d615ee11f0a2c4b6
choubung <choubung@sookmyung.ac.kr>
2025-11-09 17:31:06 +0900

    refactor(dto): LocalDateTime을 Json으로 변경하기 위한 어노테이션 추가

--- src/main/java/com/precourse/openMission/web/dto/memo/MemoUpdateRequestDto.java
+++ src/main/java/com/precourse/openMission/web/dto/memo/MemoUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.precourse.openMission.web.dto.memo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.precourse.openMission.domain.memo.MemoScope;
 import com.precourse.openMission.domain.user.User;
 import lombok.Builder;
@@ -13,10 +14,12 @@ import java.time.LocalDateTime;
 public class MemoUpdateRequestDto {
     private String content;
     private MemoScope scope;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime memoDate;
 
     @Builder
-    public MemoUpdateRequestDto(String content, User user, MemoScope scope, LocalDateTime memoDate) {
+    public MemoUpdateRequestDto(String content, MemoScope scope, LocalDateTime memoDate) {
         this.content = content;
         this.scope = scope;
         this.memoDate = memoDate;
--- src/test/java/com/precourse/openMission/web/dto/MemoSaveRequestDtoTest.java
+++
@@ -1,5 +0,0 @@
-package com.precourse.openMission.web.dto;
-
-public class MemoSaveRequestDtoTest {
-    // TODO: toEntity 단위테스트
-}

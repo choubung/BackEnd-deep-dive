dfbdac2c9b5ddc610a905304f8c725c456110d85
choubung <choubung@sookmyung.ac.kr>
2025-11-12 01:43:48 +0900

    feat: 뷰를 위한 머스테치 파일들 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,32 +1,35 @@
 var main = {
     init : function () {
         var _this = this;
-
-        // 1. '저장' 페이지이므로 #btn-save 이벤트만 남깁니다.
         $('#btn-save').on('click', function () {
             _this.save();
         });
+
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
     save : function () {
-        // 2. DTO에 맞게 데이터 수집
+        // 1. [수정] DTO에 맞게 데이터 수집
         var data = {
             content: $('#content').val(),
             scope: $('input[name="scope"]:checked').val(),
             memoDate: $('#memoDate').val()
         };
 
-        // 3. 날짜 유효성 검사
+        // 1-1. 날짜 유효성 검사
         if (!data.memoDate) {
             alert('날짜를 입력해주세요.');
             return;
         }
 
-        var token = $("meta[name='_csrf']").attr("content");
-        var header = $("meta[name='_csrf_header']").attr("content");
-
         $.ajax({
             type: 'POST',
-            url: '/home/memos',
+            url: '/home/memos', // 2. [수정] MemoController URL
             dataType: 'json',
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
@@ -34,12 +37,55 @@ var main = {
             alert('메모가 등록되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
-            // 5. GlobalExceptionHandler의 에러 메시지 표시
+            // 3. [수정] 에러 메시지 개선 (GlobalExceptionHandler와 연동)
+            alert(error.responseJSON.message || JSON.stringify(error));
+        });
+    },
+    update : function () {
+        // 4. [수정] DTO에 맞게 데이터 수집 (MemoUpdateRequestDto)
+        var data = {
+            content: $('#content').val(),
+            scope: $('input[name="scope"]:checked').val(),
+            memoDate: $('#memoDate').val()
+        };
+
+        // 4-1. 날짜 유효성 검사
+        if (!data.memoDate) {
+            alert('날짜를 입력해주세요.');
+            return;
+        }
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/home/memos/'+id, // 5. [수정] MemoController URL
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('메모가 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(error.responseJSON.message || JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/home/memos/'+id, // 6. [수정] MemoController URL
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('메모가 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
             alert(error.responseJSON.message || JSON.stringify(error));
         });
     }
 
-    // 6. 'update'와 'delete' 기능은 이 파일에서 제거합니다.
 };
 
 main.init();
